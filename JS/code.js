420c83cf62901760098e2d47ead5b1fb58d81db3
Francisco Perez <fperezdellarte@gmail.com>
2022-05-08 15:17:09 -0300

    A ver su dni

---
+++ JS/code.js
@@ -0,0 +1,15 @@
+let exit = 0
+while(exit!=1){
+    let dni =prompt("Digite su DNI");
+    if (dni.length===8 || dni.length===7) {
+        if (dni!=0000000 && dni !=00000000) {
+            console.log('Su dni es ' + dni);
+            exit+=1;
+        }else {
+            alert("su dni no es valido por favor vuelva a ingresarlo");
+        }
+    }else {
+        alert("su dni no es valido por favor vuelva a ingresarlo");
+    }
+}
+console.log("Que tenga un buen dia");
